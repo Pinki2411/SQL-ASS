2af2635877aadfc4569004fe51ef87479fe350f8
abc <abc@gmail.com>
2022-10-13 19:55:01 +0530

    First Commit

---
+++ SQL assignment 1.sql
@@ -0,0 +1,8 @@
+ Select* from Customertable
+
+Select Country from Customertable 
+where Country LIKE 'I%' OR Country LIKE 'A%'
+
+Select Firstname from Customertable where Firstname  LIKE '__i%'
+
+
